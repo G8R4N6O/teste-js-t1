6dbf1cf5582ddecf7b59b7847cfe6d8151d80dcf
branco gv <gbranco979@gmail.com>
2022-01-16 02:49:00 -0300

    update projet

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = "maria";
+let numero = "6";
+let ano = "2022";
+
+
+console.log(ano - numero);
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = false;
+console.log(quartaFeira)
